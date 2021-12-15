0b4d98a3416c61b233b4fb0ef2978ecfe4ebcd76
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-12-15 10:18:14 +0800

    Prototype D6P2 optimization

--- 62.py
+++ 62.py
@@ -1,22 +1,33 @@
 raw_data_string = "5,1,1,3,1,1,5,1,2,1,5,2,5,1,1,1,4,1,1,5,1,1,4,1,1,1,3,5,1,1,1,1,1,1,1,1,1,4,4,4,1,1,1,1,1,4,1,1,1,1,1,5,1,1,1,4,1,1,1,1,1,3,1,1,4,1,4,1,1,2,3,1,1,1,1,4,1,2,2,1,1,1,1,1,1,3,1,1,1,1,1,2,1,1,1,1,1,1,1,4,4,1,4,2,1,1,1,1,1,4,3,1,1,1,1,2,1,1,1,2,1,1,3,1,1,1,2,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,2,1,1,2,3,1,2,1,1,4,1,1,5,3,1,1,1,2,4,1,1,2,4,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,4,3,1,2,1,2,1,5,1,2,1,1,5,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,3,1,1,5,1,1,1,1,5,1,4,1,1,1,4,1,3,4,1,4,1,1,1,1,1,1,1,1,1,3,5,1,3,1,1,1,1,4,1,5,3,1,1,1,1,1,5,1,1,1,2,2"
 
-fishes = []
+fishes = {}
+for x in range(0, 11):
+    fishes[x] = 0
 for x in raw_data_string.split(","):
-    fishes.append(int(x))
+    fishes[int(x)] += 1
+
+print(fishes)
 
 
 def update():
-    for x in range(len(fishes)):
-        if fishes[x] <= 0:
-            fishes[x] = 6
-            fishes.append(8)
-        else:
-            fishes[x] -= 1
+    # Move all dict values down by 1
+    # Add keys at 0 to 8
+
+    for x in fishes.copy():
+        if x < 0:
+            continue
+        fishes[x-1] = fishes[x]
+
+    fishes[8] += fishes[0]
+    fishes[6] += fishes[0]
+    fishes[0] = 0
+
+    print(fishes)
 
 
 for i in range(256):
     print(i)
     update()
 
-print(len(fishes))
+print(sum(fishes.values()))
 # pain in the ass
