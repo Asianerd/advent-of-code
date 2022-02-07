622c8fa2415f0ce2fb048e11dde075eb2053b0d4
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-02-08 03:18:46 +0800

    D8P1, 2

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print('163cm'[-2:])
-print('163cm'[:-2])
+collection = [12, 3, 4, 1, 3, 4, 1, 2, 4, 6, 7, 3, 7, 6, 34, 2, 4, 5, 6, 2556, 34, 2, 1, 414, 23, 5, 46]
+collection.sort()
+
+print(collection)
+
+for x in [i for i in collection if (i % 2 == 0)]:
+    x += 1
+
+print(collection)
