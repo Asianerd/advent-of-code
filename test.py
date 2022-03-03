32e890ab2d502d0f7e53703a94c2755d9133259b
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-03-03 11:28:02 +0800

    D18 - D19

--- test.py
+++ test.py
@@ -1,53 +1,11 @@
-from itertools import product
-
-def neighbors(coords):
-	ranges = ((c - 1, c, c + 1) for c in coords)
-	yield from product(*ranges)
-
-def all_neighbors(cube):
-	return set(n for p in cube for n in neighbors(p))
-
-def alive_neighbors(cube, coords):
-	return sum(p in cube for p in neighbors(coords)) - (coords in cube)
-
-def evolve(cube):
-	new = set()
-
-	for p in all_neighbors(cube):
-		alive = alive_neighbors(cube, p)
-
-		if alive == 3 or (alive == 2 and p in cube):
-			new.add(p)
-
-	return new
+def reduce(iterable):
+    result = ''
+    for x in iterable:
+        if type(x) == list:
+            result += reduce(x)
+        else:
+            result += str(x)
+    return result
 
 
-raw_data_string = """..##.##.
-#.#..###
-##.#.#.#
-#.#.##.#
-###..#..
-.#.#..##
-#.##.###
-#.#..##.
-""".strip().split('\n')
-fin = raw_data_string
-
-grid = tuple(map(str.rstrip, fin))
-h, w = len(grid), len(grid[0])
-cube = set((x, y, 0) for x, y in product(range(h), range(w)) if grid[x][y] == '#')
-
-for _ in range(6):
-	cube = evolve(cube)
-
-total_alive = len(cube)
-print(total_alive)
-
-
-hypercube = set((x, y, 0, 0) for x, y in product(range(h), range(w)) if grid[x][y] == '#')
-
-for _ in range(6):
-	hypercube = evolve(hypercube)
-
-total_alive = len(hypercube)
-print(total_alive)
+print(reduce([0, [1, [0, [1, 0, 1]]], [[[0, 1], 0, [1, [0, [1, 0]]]], 1], 0]))
