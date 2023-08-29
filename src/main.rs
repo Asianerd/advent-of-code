692d09a63df4b1c196b15c0a1903f3629b73b4fc
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2023-08-29 23:17:43 +0800

    D7P1, 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod challenge_input;
-mod d6;
+mod d7;
 
 fn main() {
     // d1::p2(challenge_input::input(1));
     // d2::p2(challenge_input::input(2));
     // d3::p2(challenge_input::input(3));
     // d4::p2(challenge_input::input(4));
-    //d5::p2(challenge_input::input(5));
-    d6::p2(challenge_input::input(6));
+    // d5::p2(challenge_input::input(5));
+    // d6::p2(challenge_input::input(6));
+    d7::p2(challenge_input::input(7));
 }
